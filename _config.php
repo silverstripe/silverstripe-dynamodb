bb49230b9f79888ae6222a0f5314f127f05f6a41
Sean Harvey <sean@silverstripe.com>
2015-07-08 10:22:00 +1200

    Remove logging configuration, not the right module

--- _config.php
+++ _config.php
@@ -4,8 +4,3 @@ $dynamoSession = DynamoDbSession::get();
 if($dynamoSession) {
 	$dynamoSession->register();
 }
-
-if(defined('AWS_SYSLOG_LEVEL')) {
-	$sysLogWriter = new SS_SysLogWriter();
-	SS_Log::add_writer($sysLogWriter, (int)AWS_SYSLOG_LEVEL, '<=');
-}
